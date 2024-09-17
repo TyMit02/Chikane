e1ec3f487e05eadd1d4800713644556b2d00510a
TyMit02 <tymitchell100@gmail.com>
2024-09-16 19:22:15 -0500

    Initial Commit

---
+++ Chikane/ChikaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChikaneApp.swift
+//  Chikane
+//
+//  Created by Ty Mitchell on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ChikaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChikaneTests/ChikaneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChikaneTests.swift
+//  ChikaneTests
+//
+//  Created by Ty Mitchell on 9/16/24.
+//
+
+import Testing
+@testable import Chikane
+
+struct ChikaneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
